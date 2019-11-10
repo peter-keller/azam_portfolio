0cc55144fc3106121d46526864cd3c66f01deb56
peter-keller <keller.f.peter@gmail.com>
2019-11-10 00:33:51 +0000

    init app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <landing-block />
+  </div>
+</template>
+
+<script>
+import LandingBlock from './components/LandingBlock'
+
+export default {
+  name: 'app',
+  components: {
+    LandingBlock
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/LandingBlock.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='landing-block' :style="`background-image: url(${image})`">
+    <div class="headline">
+      <h1>{{ headline }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+      default: 'https://wallpaperaccess.com/full/405435.jpg'
+    },
+
+    headline: {
+      type: String,
+      required: false,
+      default: 'I am Azam Khan'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .landing-block {
+    width: 100vw;
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      font-size: 64px;
+      margin: 0;
+      color: $color-white;
+    }
+  }
+
+</style>
